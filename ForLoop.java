987aa2f02f6c9cb8cb6c4cd3f734e54739a9a82d
Andrea Dayo <andrealouisedayo@gmail.com>
2023-09-27 11:35:43 +0800

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 5; i += 2) {
+          System.out.println(i);
+          System.out.println("Hello World");
+        }
+    }
+}
