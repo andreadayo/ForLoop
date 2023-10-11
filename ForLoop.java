fc4a799df65204fdb79c43cf033c4cd64fbb3466
misky3 <33999758+misky3@users.noreply.github.com>
2023-10-11 21:55:58 +0800

    update on tokenizer
    wanting to add more and making sure the output of after trim and split is good. going to finish the tokenizer at friday after test.

--- ForLoop.java
+++ ForLoop.java
@@ -1,16 +1,25 @@
+import java.util.Arrays;
+
 public class ForLoop {
     public static void main(String[] args)
     {
-        for (int i = 0; i < 5; i += 2) {
+        /*for (int i = 0; i < 5; i += 2) {
           System.out.println(i);
           System.out.println("Hello World");
           System.out.println("Hello World");
-        }
+        }*/
+        String a = "Hello there. Nice to see ya.";
+        String[] test=tokenizer(a);
+        System.out.println(Arrays.toString(test));
     }
     
     //natalia
-    public String[] tokenizer(String[] a){
-      String [] b = {"1", "2"};
+    public static String[] tokenizer(String a){
+      a.trim();
+      String[] b= a.split(" "); 
+      for(int i=0; i<b.length; i++){
+        
+      }
       return b; //this is just for refresher
     }
     //natalia
